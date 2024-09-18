a3014307238005ce268bd6b118af2f18982f4286
Kauanny19 <kauannycristina01n@gmail.com>
2024-09-18 12:51:07 -0300

    18-09

--- correcaoParPrimoKauanny/src/controllers/controllerNumbers.js
+++ correcaoParPrimoKauanny/src/controllers/controllerNumbers.js
@@ -1,15 +1,24 @@
 module.exports = class controllerNumbers {
   static async checkPar(req, res) {
     const { numero } = req.body;
+    
+    if (typeof numero !== "number"){
+      return res.status(400).json({message: "Não é um número!!!"});
+    }
 
     if (numero % 2 == 0) {
       return res.status(200).json({ message: "Número Par!" });
     }
+    
     return res.status(200).json({ message: "Número Ímpar!" });
   }
 
   static async checkPrimo(req, res) {
     const { numero } = req.body;
+
+    if (typeof numero !== "number"){
+      return res.status(400).json({message: "Não é um número!!!"});
+    }
 
     if (numero <= 1) {
       return res.status(200).json({ message: "Não é Primo!" });
